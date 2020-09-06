dcdc3befb1e198b9e9c1d9e4d1e9d1c1320140db
taro.asakusa <zzuoye85@gmail.com>
2020-09-06 05:38:42 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,8 @@ include SessionsHelper
  
   def counts(user)
     @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count    
   end
   
 end
